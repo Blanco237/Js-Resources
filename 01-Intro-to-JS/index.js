58e710f478f941b4d5421b670707f79106c4089b
Blanco237 <asongrandy9@gmail.com>
2022-07-25 11:08:00 +0100

    Added Intro

---
+++ 01-Intro-to-JS/index.js
@@ -0,0 +1,9 @@
+// Javascript files are executed in order(from top to bottom).
+// The entry point for any js application is usually named index.js.
+// This file can then be executed by running node index.js.
+// or running in the browser.
+
+//The default print method in Javascript is the console.log() method.
+// console.log('Hello World');
+
+console.log('Javascript is awesome!');
